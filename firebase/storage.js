3db920eecc71649afbd8c7408d85f8e648a0c77f
Breno C <breno.cesa99.97@gmail.com>
2022-07-15 22:33:14 -0300

    Finish Features

--- firebase/storage.js
+++
@@ -1,9 +0,0 @@
-import {
-  getStorage,
-  ref,
-  uploadBytes,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-storage.js";
-
-export { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL };
